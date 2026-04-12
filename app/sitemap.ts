275452454b6f083519211bee7d666203c94fe194
Rui Gomes <ruisoft01@gmail.com>
2026-04-12 14:44:11 +0100

    melhorando SEO

---
+++ app/sitemap.ts
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://sadc-traffic-api.vercel.app/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://sadc-traffic-api.vercel.app/#api",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://sadc-traffic-api.vercel.app/#sinais",
+      lastModified: new Date(),
+    },
+  ];
+}
